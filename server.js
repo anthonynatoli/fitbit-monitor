efb501bcfa76511517271f2ffa77dd63b5b1c234
Anthony Natoli <anatoli@purdue.edu>
2015-04-18 00:14:00 -0400

    Authentication kind of works now

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var http = require('http');
 var exhbs = require( 'express3-handlebars' );
-/*var csrf = require('csurf');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
-*/
+
 var router = require('./routes/router');
 //var OAuth = require('oauthio');
+
+var PORT = process.env.PORT || 3000; //for localhost testing
+var URI = 'http://localhost:' + PORT;
+if( process.env.PORT ){
+  URI = 'http://fitbitmonitor.herokuapp.com';
+}
+
+var client_key = '943e4f12e00b951032e553e41978aedc';
+var client_secret = 'ee62b24ff77d33b05ae2b8abc7c268f6';
+var fitbitClient = require('fitbit-js')(client_key, client_secret, URI, 'en_US');
 
 var app = express();
 
 var server = http.createServer(app);
-var port = 3000; //for localhost testing
 
 //OAuth.initialize('v59jTrCUjdvuOJ1UrBVKbQmcw4M', 'nedqdXU5cBVlKdS63DH87nZcEm4')
 
@@ -20,8 +28,9 @@ app.set( 'view engine', 'handlebars' );
 
 app.use( express.static( __dirname ) );
 
-/*app.use(cookieParser());
-app.use(session({
+app.use(cookieParser());
+app.use(bodyParser.json());
+/*app.use(session({
                 secret: 'keyboard cat',
                 resave: false,
                 saveUninitialized: true
@@ -33,15 +42,49 @@ app.use(function(req, res, next) {
   next();
 });
 */
+var token;
 
 app.use('/router', router);	//use router for all routes under /router/*
 
 app.get('/', function(req, res){
-  res.render( 'home' ); 		//renders the views/home.handlebars view
+  //res.render( 'home' ); 		//renders the views/home.handlebars view
+  fitbitClient.getAccessToken(req, res, function (error, newToken) {
+    console.log('In callback');
+    if(newToken) {
+      token = newToken;
+      res.writeHead(200, {'Content-Type':'text/html'});
+      res.end('<html>Now <a href="/getStuff">get stuff</a></html>');
+    }
+    //console.log('SUCCESS!!!!');
+  });
 });
 
 app.get('/sample_route', function(req, res){
 	res.render( 'route' );
+});
+
+
+app.get('/signin', function(req, res){
+  fitbitClient.getAccessToken(req, res, function (error, newToken) {
+    console.log('In callback');
+    if(newToken) {
+      token = newToken;
+      res.writeHead(200, {'Content-Type':'text/html'});
+      //res.send('<html>Now <a href="/getStuff">get stuff</a></html>');
+    }
+    //console.log('SUCCESS!!!!');
+  });
+  console.log("Here");
+});
+
+app.get('/getStuff', function (req, res) {
+  fitbitClient.apiCall('GET', '/user/-/profile.json',
+    {token: {oauth_token_secret: token.oauth_token_secret,
+             oauth_token: token.oauth_token}},
+    function(err, resp, json) {
+      if (err) return res.send(err, 500);
+      res.json(json);
+  });
 });
 
 /*app.get('/oauth/redirect', OAuth.redirect(function(result, req, res) {
@@ -57,7 +100,7 @@ app.get('/sample_route', function(req, res){
 app.get('/signin', OAuth.auth('fitbit', 'http://localhost:3000/oauth/redirect'));
 */
 
-server.listen(process.env.PORT || port, function(){
+server.listen( PORT, function(){
   console.log('listening on *:3000');
   console.log( __dirname );
 });
